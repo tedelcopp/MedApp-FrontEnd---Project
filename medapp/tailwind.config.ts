f408b26485f2c8d99957064969010d30c99476c2
tedelcopp <tomas.edelcopp@gmail.com>
2025-02-25 11:16:12 -0300

    Globals.css

--- medapp/tailwind.config.ts
+++ medapp/tailwind.config.ts
@@ -1,18 +1,34 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",  
-    "./components/**/*.{js,ts,jsx,tsx}", 
-    "./styles/globals.css", // Agrega esto si sigue sin funcionar
-  ],
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontSize: {
+        xxl: "1.75rem",
+      },
+      textDecorationOffset: {
+        2: "2px",
+      },
       colors: {
-        backgroundLight: "var(--background-light)",
-        backgroundDark: "var(--background-dark)",
+        // Light mode
+        backgroundLight: "#33684D",
+        textLight: "#E9EAE4",
+        cardBgLight: "#D64D0C",
+        cardBgLightAlt: "#F28C5A",
+        linkLight: "#ECEDFD",
+        backgroundLightAlt: "#B5D3B0",
+
+        // Dark mode
+        backgroundDark: "#111827",
+        textDark: "#e5e7eb",
+        cardBgDark: "#1f2937",
+        linkDark: "#3b82f6",
       },
     },
   },
   plugins: [],
 };
+
+export default config;
