6c763a2bd2f1772784be150f11aaeb630857778f
tedelcopp <tomas.edelcopp@gmail.com>
2025-10-07 23:43:31 -0300

    UpdateCancelButton.WIP

--- medapp/app/patients/page.tsx
+++ medapp/app/patients/page.tsx
@@ -344,7 +344,7 @@ const Patients = () => {
                 className="bg-gray-300 text-black py-2 px-4 rounded-md hover:bg-gray-400"
                 title="Cancelar"
               >
-                Cancelar
+                Salir
               </button>
 {!isViewing && (
               <button
